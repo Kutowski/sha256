f78feb0d83b485024e299a3adf1edbee2b68d414
Thorsten Brüning <thorsten@tb-net.de>
2019-01-17 02:17:13 +0100

    dritter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-// import sha256 from 'crypto-js/sha256';
+var SHA256 = require("crypto-js/sha256")
 
-// import hmacSHA512 from 'crypto-js/hmac-sha512';
-// import Base64 from 'crypto-js/enc-base64';
-
-// const message, nonce, path, privateKey;
-// const hashDigest = sha256(nonce + message);
-// const hmacDigest = Base64.stringify(hmacSHA512(path + hashDigest, privateKey));
-
-var SHA256 = require("crypto-js/sha256");
-
-const text = "Hallo"
+const text = "Gummibärchen"
 const hash = SHA256(text).toString()
 
-console.log();
-console.log("Text: ", text);
-console.log("Hash: ", hash);
+console.log()
+console.log("Text: ", text)
+console.log("Hash: ", hash)
