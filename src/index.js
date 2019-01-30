2d3e0180fcca7a9537c3d45f7f4a1c1d25c21742
Thorsten Brüning <thorsten@tb-net.de>
2019-01-30 14:42:33 +0100

    TB hat kopiert

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 var SHA256 = require("crypto-js/sha256")
 
-const text = "Gummibärchen"
+const text = "Julian"
 const hash = SHA256(text).toString()
 
 console.log()
 console.log("Text: ", text)
 console.log("Hash: ", hash)
+
+console.log("Länge: ", text.length)
+console.log("Länge: ", hash.length)
